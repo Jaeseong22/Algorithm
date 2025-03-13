738a35ea3d5766b90df9bc66a82330f88944c5df
재성 <ljs870346@gmail.com>
2025-03-13 09:54:02 +0900

    [Bronze IV] Title: $A + B - C$, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,11 @@
+def solution():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    ls = []
+
+    result = a+b-c
+    print(result)
+    print(int(str(a)+str(b))-c)
+
+solution()

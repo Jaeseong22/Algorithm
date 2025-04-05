726ad35a2cb507d5a6ab514214b489e08faddb49
재성 <ljs870346@gmail.com>
2025-04-05 19:29:48 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,31 @@
+def solution():
+    import sys
+    input = sys.stdin.readline
+
+    n, m = map(int, input().strip().split())
+
+    chess = [input().strip() for _ in range(n)]
+    
+    white_first = [
+        'WBWBWBWB',
+        'BWBWBWBW',
+        'WBWBWBWB',
+        'BWBWBWBW',
+        'WBWBWBWB',
+        'BWBWBWBW',
+        'WBWBWBWB',
+        'BWBWBWBW'
+    ]
+
+    min_paint = 64
+    
+    for j in range(n-7):
+        for i in range(m-7):
+            count = 0
+            for row in range(8):
+                for col in range(8):
+                    if chess[j+row][i+col] != white_first[row][col]:
+                        count += 1
+            min_paint = min(min_paint, count, 64-count)
+    print(min_paint)
+solution()

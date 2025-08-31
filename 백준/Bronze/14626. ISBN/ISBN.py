21e3e47f8cb6ba0fb5339bb9c22c505f1182c13b
재성 <ljs870346@gmail.com>
2025-08-31 20:31:18 +0900

    [Bronze I] Title: ISBN, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+x = input().strip()
+result = 0
+
+is_even = False
+for i in range(13):
+    if x[i] == '*':
+        if i % 2 != 0:
+            is_even = True
+        continue
+    result += int(x[i]) * (1 if i % 2 == 0 else 3)
+
+if is_even:
+    for i in range(10):
+        if (result + (i * 3)) % 10 == 0:
+            print(i)
+            break
+else:
+    print(10 - result  % 10)

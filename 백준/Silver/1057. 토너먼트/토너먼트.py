f92ae062fdda79c0c47fb5bc1abdf17d604e4348
재성 <ljs870346@gmail.com>
2025-12-04 23:36:53 +0900

    [Silver IV] Title: 토너먼트, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, a, b = map(int, input().split())
+round = 0
+while a != b:
+    a = (a + 1) // 2
+    b = (b + 1) // 2
+    round += 1
+    
+print(round)

c8804d2fa0190f1edceddea0f0d0cbb358920a01
재성 <ljs870346@gmail.com>
2025-12-25 16:53:56 +0900

    [Silver IV] Title: 정수 제곱근, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+left, right = 0, n
+res = 0
+
+while left <= right:
+    mid = (left+right) // 2
+    if mid * mid >= n:
+        res = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(res)

d963c6d9bc8bdb3780bfa1af433bc854b6799752
재성 <ljs870346@gmail.com>
2026-01-08 19:33:22 +0900

    [Silver I] Title: IOIOI, Time: 412 ms, Memory: 37404 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,23 @@
+import re
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+s = input().strip()
+
+ans = 0
+cnt = 0
+i = 0
+
+while i < m-2:
+    if s[i:i+3] == 'IOI':
+        cnt += 1
+        if cnt >= n:
+            ans += 1
+        i += 2
+    else:
+        cnt = 0
+        i += 1
+        
+print(ans)

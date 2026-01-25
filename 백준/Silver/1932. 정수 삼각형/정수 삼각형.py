d78f03597233a76020130985e4acc8faa6c7c0e8
재성 <ljs870346@gmail.com>
2026-01-25 20:27:14 +0900

    [Silver I] Title: 정수 삼각형, Time: 152 ms, Memory: 42720 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0]*len(graph[i]) for i in range(n)]
+dp[0][0] = graph[0][0]
+
+for i in range(1, n):
+    for j in range(len(graph[i])):
+        if j == 0:
+            dp[i][j] = dp[i-1][j] + graph[i][j]
+        elif j == len(graph[i]) - 1:
+            dp[i][j] = dp[i-1][j-1] + graph[i][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + graph[i][j]
+
+print(max(dp[n-1]))

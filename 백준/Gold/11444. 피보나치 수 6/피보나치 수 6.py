c58e7b4e028731ba96293732973368cb5b0079e3
재성 <ljs870346@gmail.com>
2026-03-01 17:50:50 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+p = 1000000007
+
+def mul(A, B):
+    n = len(A)
+    Z = [[0]*n for _ in range(n)]
+    
+    for row in range(n):
+        for col in range(n):
+            e = 0
+            for i in range(n):
+                e += A[row][i] * B[i][col]
+            Z[row][col] = e % p
+    return Z
+
+def square(A, B):
+    if B == 1:
+        for x in range(len(A)):
+            for y in range(len(A)):
+                A[x][y] %= p
+        return A
+    
+    tmp = square(A, B//2)
+    if B%2:
+        return mul(mul(tmp, tmp), A)
+    else:
+        return mul(tmp, tmp)
+
+fibonacci_in = [[1, 1], [1, 0]]
+print(square(fibonacci_in, n)[0][1])

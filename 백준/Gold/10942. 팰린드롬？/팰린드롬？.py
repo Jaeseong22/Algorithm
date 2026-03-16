6c3915568ab74c432df82f00c5d70a8ff3764ae8
재성 <ljs870346@gmail.com>
2026-03-17 00:04:39 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2036 ms, Memory: 63888 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+num = list(map(int, input().split()))
+M = int(input())
+dp = [[0]*N for _ in range(N)]
+
+for i in range(N):
+    dp[i][i] = 1
+    
+for i in range(N-1):
+    if num[i] == num[i+1]:
+        dp[i][i+1] = 1
+
+for length in range(3, N+1):
+    for start in range(N-length+1):
+        end = start + length - 1
+        if num[start] == num[end] and dp[start+1][end-1] == 1:
+            dp[start][end] = 1
+        
+for _ in range(M):
+    S, E = map(int, input().split())
+    print(dp[S-1][E-1])

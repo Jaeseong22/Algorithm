24c6e3dd59b57360835ce817a09bcc1ccd27109b
재성 <ljs870346@gmail.com>
2026-03-22 17:25:08 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 316 ms, Memory: 72404 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,15 +1,8 @@
-import sys
-input = sys.stdin.readline
-
-sub = 1000000007
 n = int(input())
 
-dp = [0, 1, 1, 2]
+dp= [0,1,1]
 
-if n == 0 or n == 1 or n == 2:
-    print(dp[n])
-else: 
-    for i in range(4, n+1):
-        dp.append((dp[i-1]+dp[i-2])%sub)
+for i in range(3,n+1):
+    dp.append((dp[i-1]+dp[i-2])%1000000007)
 
 print(dp[n])

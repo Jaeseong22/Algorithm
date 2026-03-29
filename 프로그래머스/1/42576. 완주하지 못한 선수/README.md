5fa21048604cd3db712118c569573966c1f85120
재성 <ljs870346@gmail.com>
2026-03-29 14:39:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.84 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 25.3 MB, 시간: 79.54 ms
+메모리: 38.1 MB, 시간: 57.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 14일 23:39:24
+2026년 03월 29일 14:39:13
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,25 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    answer = ''
+    win = {}
+    lose = {}
     
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
+    for p in participant:
+        if p not in win:
+            win[p] = 1
+        else:
+            win[p] += 1
     
-    return participant[-1]
+    for p in completion:
+        if p not in lose:
+            lose[p] = 1
+        else:
+            lose[p] += 1
+    
+    for n, w in win.items():
+        if n not in lose:
+            answer = n
+        else:
+            if lose[n] != w:
+                answer = n
+                
+    return answer

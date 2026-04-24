e6bab535c79941a9363038a507b474ec246abb62
재성 <ljs870346@gmail.com>
2026-04-24 23:17:43 +0900

    [level 2] Title: 더 맵게, Time: 1631.81 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,15 @@
+def solution(scoville, K):
+    import heapq
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        
+        c = a + (b*2)
+        heapq.heappush(scoville, c)
+        answer += 1
+    return answer
